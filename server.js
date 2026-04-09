e0de944e5577e28b296105bcff7a48c6fad6b35d
TheurgicThaps <thapelotlowana58@gmail.com>
2026-04-09 09:39:53 +0200

    created the model , controller and route for appointment.

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 require("dotenv").config();
+//Routes
+//clinic
 const clinicsRoutes = require("./routes/clinics");
 app.use("/api/clinics", clinicsRoutes);
+//appointments
+const appointmentRoutes = require('./routes/appointments');
+app.use('/appointments', appointmentRoutes);
+
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
