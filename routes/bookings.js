ba00752c5d630d23f5bd68085215429b2281b75b
mahlatseclayton <mahlatseclayton1@gmail.com>
2026-04-11 20:44:35 +0200

    migrated routers and controllers outside server file

---
+++ routes/bookings.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const bookingController = require("../controllers/BookingController");
+
+// routing the so we can use url instead of calling the function directly
+
+router.get("/availability", bookingController.getAvailability);
+router.post("/bookings", bookingController.postBooking);
+
+module.exports = router;
