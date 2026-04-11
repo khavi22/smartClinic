cffe9678fbbae2ee7389cebb2131e33eb6b0dc43
mahlatseclayton <mahlatseclayton1@gmail.com>
2026-04-11 16:59:02 +0200

    places api fixed

--- Controllers/ClinicsController.js
+++ Controllers/ClinicsController.js
@@ -1,25 +1,25 @@
 const axios = require("axios");
 
 exports.getClinics = async (req, res) => {
-  const searchName=req.query.search;
+  const searchName = req.query.search;
   const latitude = req.query.lat;
   const longitude = req.query.lon;
-  let  Query;
+  let Query;
 
   ///search name exits checking if it has a value
-  if(searchName){
-    Query = `${searchName} clinics in Johannesburg South Africa`;
+  if (searchName) {
+    Query = `clinic named ${searchName}`;
   }
   //check if latitude and longitude exist if the patient search using location
-  if(latitude &&  longitude){
-    Query =`clinic near ${latitude},${longitude} in Johannesburg South Africa`
+  if (latitude && longitude) {
+    Query = `clinic near ${latitude},${longitude}`
   }
   console.log(process.env.GOOGLE_MAPS_API_KEY);
   try {
     const response = await axios.post(
       "https://places.googleapis.com/v1/places:searchText",
       {
-        textQuery:Query 
+        textQuery: Query
       },
       {
         headers: {
