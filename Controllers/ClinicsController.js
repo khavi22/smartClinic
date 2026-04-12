c2f35f6847af22b3c5e3b5d356192c4b9dd0612d
MartinMMM100 <martinmundalamo6@gmail.com>
2026-04-12 12:04:55 +0200

    testing deployment

--- Controllers/ClinicsController.js
+++ Controllers/ClinicsController.js
@@ -29,9 +29,16 @@ exports.getClinics = async (req, res) => {
       }
     );
 
-    res.json(response.data);
+    console.log("Google success");
+    return res.json(response.data);
   } catch (err) {
-    console.error("Google Places error:", err.response?.data || err.message);
-    res.status(500).json({ error: "Error fetching clinics" });
+    console.error("Google status:", err.response?.status);
+    console.error("Google data:", JSON.stringify(err.response?.data, null, 2));
+    console.error("Google message:", err.message);
+
+    return res.status(500).json({
+      error: "Error fetching clinics",
+      details: err.response?.data || err.message
+    });
   }
 };
