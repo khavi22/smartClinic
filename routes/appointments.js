b9c2b63362f5f18ce72b35f227f2a672b1e4bbce
TheurgicThaps <thapelotlowana58@gmail.com>
2026-04-12 23:05:37 +0200

    Fixed casing error

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const appointmentsController = require("../controllers/appointmentsController");
+const appointmentsController = require("../Controllers/appointmentsController");
 
 router.get("/appointments/:patientId", appointmentsController.getAppointmentsByPatientId);
 router.get("/availability", appointmentsController.getAvailability);
