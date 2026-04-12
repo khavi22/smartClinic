ecfccf686f48245f1a9ddf63f510180749591386
MartinMMM100 <martinmundalamo6@gmail.com>
2026-04-12 14:31:13 +0200

    starting directory in index

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "dashboard.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
